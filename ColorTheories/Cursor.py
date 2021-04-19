7dfadbc4cb27bef3d398241b1cea4b76037085e0
Augusto Nunes Zacarias <augustonz2010@hotmail.com>
2021-04-19 17:02:32 -0300

    Deleted unneeded files

--- ColorTheories/Cursor.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import tools
-
-class Cursor(pygame.sprite.Sprite):
-    def __init__(self):
-        pg.sprite.Sprite.__init__(self)
-
-    def click(self,target):
-        pos = pygame.mouse.get_pos()
